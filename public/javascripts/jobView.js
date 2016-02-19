5c4af37c8b580369d51071316e6ad27498477b6c
kati taylor <katherine.pelletier@gmail.com>
2016-02-19 15:19:02 -0600

    saved ourselves maybe

---
+++ public/javascripts/jobView.js
@@ -0,0 +1,13 @@
+ (function() {
+
+   var app = angular.module('JobOut');
+   app.directive('job', function() {
+     var directive = {};
+     directive.restrict = 'E';
+     directive.replace = true;
+     directive.templateUrl =  "/templates/_listJobView.html";
+
+     return directive;
+   });
+ console.log("hello");
+ }());
---
+++ public/javascripts/jobsController.js
@@ -0,0 +1,19 @@
+(function() {
+ var app = angular.module('JobOut');
+  app.controller('JobsController', function($http) {
+   var self = this;
+
+   $http({
+     method: 'GET',
+      url: '/api/v1/jobs'
+    }).then(function successCallback(response) {
+      console.log('success', response.data.docs);
+
+      self.jobs = response.data.docs;
+    }, function errorCallback(response) {
+
+    });
+
+    return this;
+  });
+})();
