c2724d7a1c8b2412babb18bcbbd3249e7e1e60a3
Kelli Waiss <kelliwaiss@hotmail.com>
2016-02-19 16:46:11 -0600

    add search function

--- public/javascripts/jobView.js
+++ public/javascripts/jobView.js
@@ -2,7 +2,9 @@
  (function() {
 
    var app = angular.module('JobOut');
+   
    app.directive('job', function() {
+
      var directive = {};
      directive.restrict = 'E';
      directive.replace = true;
--- public/javascripts/jobsController.js
+++ public/javascripts/jobsController.js
@@ -17,4 +17,24 @@
 
     return this;
   });
+
+function loadMore(){
+
+var newPage=1;
+
+  $http({
+    method: 'GET',
+     url: '/api/v1/jobs?page=newPage'
+   }).then(function successCallback(response) {
+    newPage=newPage+1;
+    console.log('success', response.data.docs);
+
+     self.jobs = response.data.docs;
+   }, function errorCallback(response) {
+
+   });
+
+   return this;
+  };
+
 })();
